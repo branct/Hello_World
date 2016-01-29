9db7daff1f3f916beea14127fd9ee6521311c60f
branct <branct@alum.rpi.edu>
2016-01-29 08:59:56 -0500

    Revert "Tom's Changes"
    This reverts commit bbce6eec378c113ad8a3b5c6b3b9af49d0aa9cc1.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -17,7 +17,6 @@ public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("Hello World");
        System.out.println("Hello Issue12345");
-       System.out.println("I am adding Tom's Change");
     }
     
 }
